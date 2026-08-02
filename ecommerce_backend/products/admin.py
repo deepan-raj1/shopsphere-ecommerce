e3e317b3d9fc818593e785220c7741baa4b860dc
Deepak <dhpnrj@gmail.com>
2026-08-02 14:12:15 +0530

    feat: add Category model and admin configuration

--- ecommerce_backend/products/admin.py
+++ ecommerce_backend/products/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "slug",
+        "is_active",
+        "created_at",
+    )
+
+    list_filter = (
+        "is_active",
+        "created_at",
+    )
+
+    search_fields = (
+        "name",
+    )
+
+    prepopulated_fields = {
+        "slug": ("name",)
+    }
+
+    list_per_page = 20
