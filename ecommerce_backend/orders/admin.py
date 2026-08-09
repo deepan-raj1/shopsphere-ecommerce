663457afb302d94e7e08805e48e5f779854c6a23
Deepak <dhpnrj@gmail.com>
2026-08-09 13:04:25 +0530

    feat: add Address model and admin configuration

--- ecommerce_backend/orders/admin.py
+++ ecommerce_backend/orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Address
 
-# Register your models here.
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user','full_name', 'phone_number', 'city', 'state', 'postal_code', 'country', 'is_default', 'created_at',)
+
+    list_filter = ('is_default', 'country', 'state', 'city')
+
+    search_fields = ('user__email', 'full_name', 'phone_number', 'city', 'state', 'postal_code', 'country')
+    ordering = ('-is_default', '-created_at')
+
+    list_per_page = 20
+
+
+
--- ecommerce_backend/orders/models.py
+++ ecommerce_backend/orders/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Address(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='addresses')
+    full_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+
+    address_line1 = models.CharField(max_length=255)
+    address_line2 = models.CharField(max_length=255, blank=True)
+
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=100, default='India')
+
+    is_default = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Addresses"
+        ordering = ['-is_default', '-created_at']
+
+    def __str__(self):
+        return f"{self.full_name}, {self.phone_number}, {self.city}, {self.state}, {self.postal_code}, {self.country}"
+
