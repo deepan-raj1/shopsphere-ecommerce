15e8990b97d1353ab32e0b31dc32378e5334c874
Deepak <dhpnrj@gmail.com>
2026-08-10 22:14:45 +0530

    feat: add Review model with rating and duplicate protection

--- ecommerce_backend/reviews/admin.py
+++ ecommerce_backend/reviews/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id','product', 'user', 'rating', 'title', 'is_verified_purchase', 'is_active', 'created_at')
+
+    list_filter = ('rating', 'is_verified_purchase', 'is_active', 'created_at')
+
+    search_fields = ('product__name', 'user__email', 'title', 'comment')
+
+    readonly_fields = ('created_at', 'updated_at')
+
+    ordering = ('-created_at',)
+
+    list_per_page = 20
+
+
