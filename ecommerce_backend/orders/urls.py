e7f16359e3b71feabe921f1518883e5f738dd9d0
Deepak <dhpnrj@gmail.com>
2026-09-20 14:50:07 +0530

    feat: add address detail API

--- ecommerce_backend/orders/urls.py
+++ ecommerce_backend/orders/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import AddressListView
+from .views import AddressListView, AddressDetailView
 
 urlpatterns = [
     path("addresses/", AddressListView.as_view(), name="address-list"),
+    path("addresses/<int:pk>/", AddressDetailView.as_view(), name="address-detail"),
 ]
+
 
--- ecommerce_backend/orders/views.py
+++ ecommerce_backend/orders/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Address
@@ -17,5 +17,16 @@ class AddressListView(ListAPIView):
         return Address.objects.filter(
             user=self.request.user
         )
+
+class AddressDetailView(RetrieveAPIView):
+
+    serializer_class = AddressSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Address.objects.filter(
+            user=self.request.user
+        )
+
 
 
